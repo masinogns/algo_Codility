c487de8b39e5afa1410b9781100a795e16c651ae
백기훈 <masinogns@gmail.com>
2017-08-30 15:40:10 +0900

    Prefixsum 예제 풀고 돌리기 in codility pdf

---
+++ TestLesson/lesson5/PrefixSumsTest.java
@@ -0,0 +1,27 @@
+package lesson5;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by masinogns on 2017. 8. 26..
+ *
+ * Prefix sum 수열의 구간 합 구하기
+ */
+public class PrefixSumsTest {
+    PrefixSums application;
+
+    int[] A = new int[]{2,3,5,4,1,3};
+    @Before
+    public void setUp() throws Exception {
+        application = new PrefixSums();
+    }
+
+    @Test
+    public void test2(){
+        assertEquals(12, application.countTotal(application.prefixSum(A), 1, 3));
+    }
+
+}
---
+++ src/lesson5/PrefixSums.java
@@ -0,0 +1,33 @@
+package lesson5;
+
+/**
+ * Created by masinogns on 2017. 8. 26..
+ *
+ * 부분 합이란 0~k까지의 합을 의미하는 것이고
+ * 구간 합이란 a~b까지의 합을 의미하는 것이다
+ *
+ * http://www.crocus.co.kr/843
+ *
+ * https://www.acmicpc.net/problem/1806
+ * https://www.acmicpc.net/problem/1644
+ * https://www.acmicpc.net/problem/11441
+ 출처: http://www.crocus.co.kr/599 [Crocus]
+ *
+ */
+public class PrefixSums {
+    int[] P;
+
+    public int[] prefixSum(int[] A) {
+         P = new int[A.length+1];
+
+        for (int i = 1; i < A.length+1; i++){
+            P[i] = P[i-1] + A[i-1];
+        }
+        return P;
+    }
+
+    // P[x] + P[x+1] + ... + P[y-1] + P[y]
+    public int countTotal(int[] P, int x, int y){
+        return P[y+1] - P[x];
+    }
+}
